59b1444b9c6fcc2df17c191824d92ed829feb50d
dansims2024-afk <dansims.2024@gmail.com>
2025-12-30 10:00:11 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// The "./" tells the computer to look in the same "src" folder for App.jsx
+import App from './App.jsx' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
