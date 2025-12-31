3bbd55f5e7411135fa9f1248bf2ec48c47eec596
dansims2024-afk <dansims.2024@gmail.com>
2025-12-31 06:41:27 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// The './' is essential—it tells Vite to look in the SAME folder (src)
-import App from './App.jsx' 
+import App from './App.jsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
