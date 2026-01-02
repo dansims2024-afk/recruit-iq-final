48d822fb11dbb77fef041613052fe2830aa4f4c8
dansims2024-afk <dansims.2024@gmail.com>
2026-01-02 15:08:59 -0500

    Update LandingPage.jsx

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,8 +1,8 @@
 export default function LandingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#020617] text-white">
-      <h1 className="text-5xl font-black">Recruit-IQ</h1>
-      <p className="mt-4 text-slate-400">Sign in to start your 3 free screens.</p>
+      <h1 className="text-6xl font-black mb-4">Recruit-IQ</h1>
+      <p className="text-xl text-slate-400">Sign in to start your 3 free screens.</p>
     </div>
   );
 }
