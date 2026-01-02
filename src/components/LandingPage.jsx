fee1e6d3a729cedcb89fec4d30c0f7d7bf22007b
dansims2024-afk <dansims.2024@gmail.com>
2026-01-02 15:06:20 -0500

    Update and rename Landingpage.jsx to LandingPage.jsx

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,8 @@
+export default function LandingPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#020617] text-white">
+      <h1 className="text-5xl font-black">Recruit-IQ</h1>
+      <p className="mt-4 text-slate-400">Sign in to start your 3 free screens.</p>
+    </div>
+  );
+}
