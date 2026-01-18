47f66e5627222022b444cb979d38476227f31a75
dansims2024-afk <dansims.2024@gmail.com>
2026-01-18 18:30:36 -0500

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // <--- MAKE SURE THIS LINE IS HERE
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
