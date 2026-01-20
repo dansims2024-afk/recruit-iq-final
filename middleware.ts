69ac2f6554f297ac1e6e378632ce87787425ffef
dansims2024-afk <dansims.2024@gmail.com>
2026-01-20 15:19:12 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// This allows the home page to be accessible to guests
 export default authMiddleware({
+  // This ensures the middleware doesn't try to protect 
+  // the home page or static assets, avoiding extra network calls.
   publicRoutes: ["/"],
 });
 
 export const config = {
-  // Protects all routes, including api/trpc.
-  // See https://clerk.com/docs/references/nextjs/auth-middleware
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
