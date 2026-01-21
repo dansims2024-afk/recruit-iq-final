48e6e78d147ce10bb54bc5897182a58fbf373bbf
dansims2024-afk <dansims.2024@gmail.com>
2026-01-21 13:23:42 -0500

    Update route.ts

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,31 +1,38 @@
-import { clerkClient } from "@clerk/nextjs";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { Webhook } from "svix";
 import Stripe from "stripe";
+import { clerkClient } from "@clerk/nextjs";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2023-10-16",
 });
 
 export async function POST(req: Request) {
-  const body = await req.text();
-  const signature = req.headers.get("Stripe-Signature") as string;
+  const payload = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
   let event: Stripe.Event;
 
   try {
-    event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET!);
-  } catch (error: any) {
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+    if (!webhookSecret) return new NextResponse("Missing Webhook Secret", { status: 500 });
+    event = stripe.webhooks.constructEvent(payload, signature, webhookSecret);
+  } catch (err: any) {
+    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
   }
 
-  const session = event.data.object as Stripe.Checkout.Session;
-
+  // LISTENER: If payment is successful, UNLOCK the user
   if (event.type === "checkout.session.completed") {
-    const clerkUserId = session.client_reference_id;
-    if (clerkUserId) {
-      await clerkClient.users.updateUserMetadata(clerkUserId, {
+    const session = event.data.object as Stripe.Checkout.Session;
+    const userId = session.client_reference_id;
+
+    if (userId) {
+      await clerkClient.users.updateUser(userId, {
         publicMetadata: { isPro: true },
       });
     }
   }
-  return new NextResponse(null, { status: 200 });
+
+  return new NextResponse("Success", { status: 200 });
 }
