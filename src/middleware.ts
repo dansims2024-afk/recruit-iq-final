750333d86d9d485236d9a55d55bc3317f5ec0277
dansims2024-afk <dansims.2024@gmail.com>
2026-01-23 09:47:42 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,11 +3,12 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Allows the home page and stripe webhook to be viewed without being logged in
   publicRoutes: ["/", "/api/webhook/stripe"],
+  
   // Prevents the middleware from blocking these essential background routes
   ignoredRoutes: ["/api/webhook/stripe", "/(api|trpc)(.*)"],
 });
 
 export const config = {
-  // This ensures Clerk's internal redirect files aren't treated as "missing pages"
+  // This standard matcher ensures Clerk's internal redirect files aren't treated as "missing pages"
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
