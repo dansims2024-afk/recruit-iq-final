efd8c5d5da7719b6d64c4ba61e7d1fe5217da859
dansims2024-afk <dansims.2024@gmail.com>
2026-01-23 14:29:22 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // "PASSIVE MODE": Allow all routes so the dashboard can handle logic.
-  // This stops the middleware from fighting with the redirect loop.
   publicRoutes: ["/(.*)"],
 });
 
 export const config = {
-  // Standard matcher to allow Clerk internal scripts to run
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
