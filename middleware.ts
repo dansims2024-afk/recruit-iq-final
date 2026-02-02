5e0294dd763b19b762867698df6261bc01926650
dansims2024-afk <dansims.2024@gmail.com>
2026-02-02 09:47:06 -0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
+  // Allows the landing page and authentication routes to be accessed without logging in
   publicRoutes: ["/", "/sign-in(.*)", "/sign-up(.*)", "/api/webhook(.*)"],
 });
 
 export const config = {
+  // Protects all routes except static files and next internals
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
