a48a458b1d336c49a8f773d35bfb0565433172d0
dansims2024-afk <dansims.2024@gmail.com>
2026-02-02 15:08:34 -0500

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,7 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-import Dashboard from "../../components/Dashboard";
+import { auth } from "@clerk/nextjs/server";
+import Dashboard from "@/components/Dashboard";
 
-export default async function DashboardPage() {
-  const { userId } = auth();
-
-  // Standard redirect if the user is not signed in
-  if (!userId) {
-    redirect("/sign-in");
-  }
-
+export default function DashboardPage() {
+  auth().protect();
   return <Dashboard />;
 }
